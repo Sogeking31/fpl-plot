e89b574252324796207b39049a35ada144f5490c
Soge972 <92600093+Soge972@users.noreply.github.com>
2022-04-07 23:08:49 +0300

    plot first draft

--- fpl_plot.py
+++ fpl_plot.py
@@ -1,22 +1,38 @@
-
+import matplotlib.pyplot as plt
 file_name = 'fplrecord.txt'
+data = []
 with open (file_name, 'r') as f:
 	lines = f.readlines()
+	for line in lines:
+		stripped_line = line.strip()
+		line_list = stripped_line.split()
 
-oarank = lines[1]
-gwrank = lines[3]
-gwpoints = lines[5]
-oapoints = lines[7]
-tv = lines[9]
-print(f"{oarank}\n{gwrank}\n{gwpoints}\n{oapoints}\n{tv}")
+		data.append(line_list)
 
-#plt.style.use('seaborn')
-#fig, ax = plt.subplots()
-#ax.plot(range(31), oarank, linewidth = 3)
-#ax.set_title("FPL Rank", fontsize= 24)
-#ax.set_xlabel("GW", fontsize=14)
-#ax.set_ylabel("rank", fontsize = 14)
-#a#x.tick_params(axis='both', labelsize= 14)
-#plt.show()
+oarank = data[1]
+gwrank = data[3]
+gwpoints = data[5]
+oapoints = data[7]
+tv = data[9]
 
-#p#rint(tv)
+oarank = [x.replace(',','') for x in oarank]
+oarank = [x.replace('[','') for x in oarank]
+oarank = [x.replace(']','') for x in oarank]
+oarank = [x.replace("'","") for x in oarank]
+
+oarank = [int(x) for x in oarank]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(range(31), oarank, linewidth = 2)
+ax.set_title("FPL Rank", fontsize= 24)
+ax.set_xlabel("GW", fontsize= 14)
+ax.set_ylabel("rank", fontsize = 14)
+ax.tick_params(axis='both', labelsize= 14)
+plt.xticks(range(31))
+plt.yscale('log')
+ticks= (500000, 250000, 100000, 50000, 25000, 10000, 5000)
+plt.yticks(ticks)
+ax.set_yticklabels(["500k", "250k", "100k", "50k", '25k', '10k', '5k'])
+plt.show()
+
