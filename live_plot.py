f11a2224ce2f014dd2b4a75fbcd54c8d9d3ea647
Soge972 <92600093+Soge972@users.noreply.github.com>
2022-04-08 11:19:05 +0300

    improvements

--- live_plot.py
+++ live_plot.py
@@ -1,4 +1,7 @@
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 file_name = 'fplrecord.txt'
 data = []
 with open (file_name, 'r') as f:
@@ -22,17 +25,29 @@ oarank = [x.replace("'","") for x in oarank]
 
 oarank = [int(x) for x in oarank]
 
-plt.style.use('seaborn')
-fig, ax = plt.subplots(figsize=(16,6))
-ax.plot(range(31), oarank, linewidth = 2)
+plt.style.use('seaborn-darkgrid')
+fig, ax = plt.subplots(1,1,figsize=(12,6))
+x=[]
+y=[]
+count =0
+def animate(i):
+	global count
+	x.append(count)
+	y.append(oarank[count])
+	count += 1
+	plt.plot(x,y, color = 'orange')
+	plt.yscale('log')
+	ticks= (500000, 250000, 100000, 50000, 25000, 10000, 5000)
+	plt.yticks(ticks)
+	ax.set_yticklabels(["500k", "250k", "100k", "50k", '25k', '10k', '5k'])
+
+	plt.locator_params(axis="x", nbins=count)
+
 ax.set_title("OR 2021/2022 Season", fontsize= 24)
+ax.tick_params(axis='both', labelsize= 14)
 ax.set_xlabel("GameWeek ", fontsize= 14)
 ax.set_ylabel("Overall Rank", fontsize = 14)
-ax.tick_params(axis='both', labelsize= 14)
-plt.xticks(range(31))
-plt.yscale('log')
-ticks= (500000, 250000, 100000, 50000, 25000, 10000, 5000)
-plt.yticks(ticks)
-ax.set_yticklabels(["500k", "250k", "100k", "50k", '25k', '10k', '5k'])
+ani = animation.FuncAnimation(plt.gcf(), animate, interval=1000)
+plt.gca().invert_yaxis()
+ax.yaxis.tick_right()
 plt.show()
-
